d4d15e99586d1cdd9768a361a281cd16b59cd835
Todor B. Mihaylov <tbmihailov@gmail.com>
2014-02-05 00:53:02 +0200

    OpenWeatherMap api projects

---
+++ src/FishActivityFuzzyCalculator/OpenWeatherMap.Tests/OpenWeatherMapClient_Tests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text;
+using OpenWeatherMap;
+using Common.Helpers;
+
+namespace FishActivityFuzzyCalculator.Tests
+{
+    [TestClass]
+    public class OpenWeatherMapClient_Tests
+    {
+        [TestMethod]
+        public void Get_Sozopol_History_Test()
+        {
+            string sozopolId = "726963";//from openstreetmap.org
+            OpenWeatherMapClient owmClient = new OpenWeatherMapClient();
+
+            DateTime fromDate = new DateTime(2013, 1, 1);
+            DateTime toDate = new DateTime(2013, 4, 1);
+            owmClient.GetHistoryDataAsync(sozopolId, fromDate, toDate,
+                (a) =>
+                {
+                    var data = a.Data;
+                    string cityId = data.city_id.ToString();
+
+                    if (data.list != null)
+                    {
+                        foreach (var item in data.list)
+                        {
+                            foreach (var weather in item.weather)
+                            {
+                                Debug.WriteLine("{0}", DateHelper.FromUnixTimestamp(item.dt).ToString("yyyy-MM-dd"));
+                            }
+                        }
+                    }
+                });
+        }
+
+        public void test_get_from_2008_2013()
+        {
+            string sozopolId = "726963";//from openstreetmap.org
+            OpenWeatherMapClient owmClient = new OpenWeatherMapClient();
+
+            DateTime firstDate = new DateTime(2008, 1, 1);
+            int pageLengthInMonths = 3;
+            int pages = 4;// *6;
+
+            List<WeatherList> allCalls = new List<WeatherList>();
+            for (int i = 0; i < pages; i++)
+            {
+                DateTime fromDate = firstDate.AddMonths(i * pageLengthInMonths);
+                DateTime toDate = firstDate.AddMonths((i + 1) * pageLengthInMonths);
+
+                var data = owmClient.GetHistoryData(sozopolId, fromDate, toDate);
+
+                string cityId = data.city_id.ToString();
+                if (data.list != null)
+                {
+                    //foreach (var item in data.list)
+                    //{
+                    //    Debug.WriteLine("{0}", DateHelper.FromUnixTimestamp(item.dt).ToString("yyyy-MM-dd-HH-mm"));
+                    //}
+
+                    allCalls.AddRange(data.list);
+                }
+            }
+
+            JsonWriter writer = new JsonTextWriter(new StreamWriter("Sozopol_Weather.json", false, Encoding.UTF8));
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Serialize(writer, allCalls);
+
+
+        }
+    }
+}
---
+++ src/FishActivityFuzzyCalculator/OpenWeatherMap/OpenWeatherMapClient.cs
@@ -0,0 +1,60 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Helpers;
+
+namespace OpenWeatherMap
+{
+    public class OpenWeatherMapClient
+    {
+        const string PARAM_AUTH_TOKEN = "AUTH_TOKEN";
+        string _accessToken;
+        string _apiUrl;
+
+        public OpenWeatherMapClient(string apiUrl = "http://api.openweathermap.org/data/2.5/")
+            : this(apiUrl, string.Empty)
+        {
+
+        }
+
+        public OpenWeatherMapClient(string apiUrl, string accessToken)
+        {
+            _apiUrl = apiUrl;
+            _accessToken = accessToken;
+        }
+
+        public void GetHistoryDataAsync(string cityId, DateTime fromDate, DateTime toDate, Action<IRestResponse<RootObject>> callback)
+        {
+            RestClient client;
+            RestRequest request;
+            PrepareGetHistoryData(cityId, fromDate, toDate, out client, out request);
+
+            client.ExecuteAsync(request, callback);
+        }
+
+        public RootObject GetHistoryData(string cityId, DateTime fromDate, DateTime toDate)
+        {
+            RestClient client;
+            RestRequest request;
+            PrepareGetHistoryData(cityId, fromDate, toDate, out client, out request);
+
+            var response = client.Execute<RootObject>(request);
+            return response.Data;
+        }
+
+        private void PrepareGetHistoryData(string cityId, DateTime fromDate, DateTime toDate, out RestClient client, out RestRequest request)
+        {
+            client = new RestClient(_apiUrl);
+
+            //"history/city?id=726963&type=day&start=1369728000&end=1369789200"
+            string fromDateStr = fromDate.ToUnixTimestamp().ToString();
+            string toDateStr = toDate.ToUnixTimestamp().ToString();
+            string resourceUrl = string.Format("history/city?id={0}&type=day&start={1}&end={2}", cityId, fromDateStr, toDateStr);
+            request = new RestRequest(resourceUrl, Method.GET);
+            request.RequestFormat = DataFormat.Json;
+        }
+    }
+}
